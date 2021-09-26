40513a268fc344da722ed686ea40c20721b478cd
Fola Flor <dpcalfola@gmail.com>
2021-09-26 20:51:56 +0900

    Curriculum #4.0 Input Values
    to make login form,
    how to get a form value

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
 console.log("test");
+const loginForm = document.querySelector(".login_form");
+
+const loginInput = loginForm.querySelector("input");
+const loginbutton = loginForm.querySelector("button");
+
+function handleLoginBtnClick() {
+  console.log("click!!");
+  // console.dir(loginInput);
+  console.log("Hello!!", loginInput.value);
+}
+
+loginbutton.addEventListener("click", handleLoginBtnClick);
