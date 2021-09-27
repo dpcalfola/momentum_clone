0b3736fcecbde300cfe65d990aca55f7a718fcb8
Fola Flor <dpcalfola@gmail.com>
2021-09-27 16:57:25 +0900

    Curriculum #4.3 Events part Two, #4.4 Getting Username
    Change css style using JS. Especially hidden or display html contents
    Getting summit value and showing on html document

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 // console.log("test");
 const loginForm = document.querySelector(".login_form");
-
 const loginInput = loginForm.querySelector("input");
+const loginGreeting = document.querySelector(".greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
+  const userName = loginInput.value;
+  console.log(userName);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  loginGreeting.classList.remove(HIDDEN_CLASSNAME);
 
-  console.log(loginInput.value);
+  loginGreeting.innerText = `Wellcome ${userName} !!`;
+  console.log(document);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
--- src/js_script_code_memo.js
+++ src/js_script_code_memo.js
@@ -1,15 +1,39 @@
 // ========????  code : how to check up vlaue length    ????==========
+
+function handleLoginBtnClick() {
+  const userName = loginInput.value;
+  if (userName.length > 15) {
+    console.log("Momentum error : Too long name");
+  } else if (userName === "") {
+    console.log("Please write your name");
+  } else {
+    console.log("hello", userName);
+  }
+}
+loginbutton.addEventListener("click", handleLoginBtnClick);
 //
-// function handleLoginBtnClick() {
-//   const userName = loginInput.value;
-//   if (userName.length > 15) {
-//     console.log("Momentum error : Too long name");
-//   } else if (userName === "") {
-//     console.log("Please write your name");
-//   } else {
-//     console.log("hello", userName);
-//   }
-// }
-// loginbutton.addEventListener("click", handleLoginBtnClick);
 //
-// ========????  code : how to check up vlaue length    ????==========
+//
+
+//
+// ========????  code : about preventDefault() function  ????==========
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  console.dir(event);
+}
+
+const linkClick = document.querySelector("a");
+linkClick.addEventListener("click", handleLinkClick);
+//
+//
+//
+
+// ========????  code : both codes are same  ????==========
+//
+loginGreeting.innerText = "Wellcome " + userName + " !!";
+loginGreeting.innerText = `Wellcome ${userName} !!`;
+// ` <-- backtick : left of 'NUM1' keypad
+//
+//
+//
